8a24549cbad9bcaacb8a454e8473738e9e45a042
ritikv6 <ritikverma5767@gmail.com>
2025-08-15 14:12:32 +0530

    update_0.1

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Footer.css'
-import footer_logo from '../../assets/footer_logo.svg'
+import footer_logo from '../../assets/logo1.png'
 import user_icon from '../../assets/user_icon.svg'
 const Footer = () => {
     return (
